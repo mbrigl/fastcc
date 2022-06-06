d3aaeebeeb9ef01f3a0fb04314811c94ec1af7ee
Markus Brigl <markus.brigl@tol.info>
2022-06-06 08:10:05 +0200

    Create a Custom Main

---
+++ src/main/java/org/fastcc/FastCCBuilder.java
@@ -0,0 +1,133 @@
+package org.fastcc;
+
+
+import org.javacc.jjtree.JJTree;
+import org.javacc.parser.Main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The {@link FastCCBuilder} class.
+ */
+public class FastCCBuilder {
+
+  public enum Language {
+
+    Java("Java"),
+    Cpp("C++");
+
+    public final String name;
+
+    private Language(String name) {
+      this.name = name;
+    }
+
+  }
+
+  private Language language;
+  private File     outputDirectory;
+
+  private File     jj;
+  private File     jjt;
+
+  /**
+   * Set the code generator.
+   *
+   * @param language
+   */
+  public final FastCCBuilder setCodeGenerator(Language language) {
+    this.language = language;
+    return this;
+  }
+
+  /**
+   * Set the output directory.
+   *
+   * @param outputDirectory
+   */
+  public final FastCCBuilder setOutputDirectory(File outputDirectory, String... pathes) {
+    this.outputDirectory = FastCCBuilder.toFile(outputDirectory, pathes);
+    return this;
+  }
+
+  /**
+   * Set the jj file.
+   *
+   * @param file
+   */
+  public final FastCCBuilder setJJFile(File file, String... pathes) {
+    this.jj = FastCCBuilder.toFile(file, pathes);
+    return this;
+  }
+
+  /**
+   * Set the jj file.
+   *
+   * @param file
+   */
+  public final FastCCBuilder setJJTreeFile(File file, String... pathes) {
+    this.jjt = FastCCBuilder.toFile(file, pathes);
+    return this;
+  }
+
+  public static FastCCBuilder of(Language language) {
+    FastCCBuilder builder = new FastCCBuilder();
+    builder.setCodeGenerator(language);
+    return builder;
+  }
+
+  /**
+   * Run the parser generator.
+   */
+  public final void build() {
+    try {
+      List<String> arguments = new ArrayList<>();
+      arguments.add("-CODE_GENERATOR=" + this.language.name);
+      arguments.add("-OUTPUT_DIRECTORY=" + this.outputDirectory.getAbsolutePath());
+      arguments.add("-JJTREE_OUTPUT_DIRECTORY=" + this.outputDirectory.getAbsolutePath());
+      if (this.jjt != null) {
+        arguments.add(jjt.getAbsolutePath());
+        String path = jjt.getAbsolutePath();
+
+        JJTree jjtree = new JJTree();
+        jjtree.main(arguments.toArray(new String[arguments.size()]));
+
+        int offset = path.lastIndexOf("/");
+        int length = path.lastIndexOf(".");
+        arguments.set(arguments.size() - 1, this.outputDirectory + path.substring(offset, length) + ".jj");
+      } else {
+        arguments.add(this.jj.getAbsolutePath());
+      }
+
+      Main.mainProgram(arguments.toArray(new String[arguments.size()]));
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static File toFile(File file, String... pathes) {
+    return (pathes.length == 0) ? file : new File(file, String.join(File.separator, pathes));
+  }
+
+  public static final File USER = new File(System.getProperty("user.dir"));
+  public static final File BASE = new File("/data/smartIO/develop/parser/parser/src/main/resources/it/smartio/text");
+
+  /**
+   * {@link #main}.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    FastCCBuilder builder = FastCCBuilder.of(Language.Cpp);
+    builder.setOutputDirectory(FastCCBuilder.BASE, "cpp");
+    builder.setJJTreeFile(FastCCBuilder.BASE, "parser/OQL-Cpp.jjt");
+    builder.build();
+
+    builder = FastCCBuilder.of(Language.Java);
+    builder.setOutputDirectory(FastCCBuilder.BASE, "java");
+    builder.setJJTreeFile(FastCCBuilder.BASE, "parser/OQL-Java.jjt");
+    builder.build();
+  }
+}
